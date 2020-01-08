79c6a316dac12190991a6ac4820b8c5e0ed7c0d3
Ankit Srivastava <asrivast28@gmail.com>
2020-01-08 11:41:05 -0500

    Added an option for chaining in the PBS script generator.

--- scripts/pbs.py
+++ scripts/pbs.py
@@ -21,6 +21,7 @@ def parse_args():
     parser.add_argument('-N', '--nodes', type=int, default=1, metavar='NODES', help='Number of required nodes.')
     parser.add_argument('-q', '--queue', type=str, default='hive', metavar='NAME', help='Name of the queue.')
     parser.add_argument('-o', '--output', type=str, metavar='FILE', help='Name of the output file.')
+    parser.add_argument('-d', '--depend', type=str, metavar='JOBID', help='ID of the job on which this job depends.')
     args = parser.parse_args()
     return args
 
@@ -34,17 +35,20 @@ def create_submission_script(args):
 
     preamble_format = \
 '''\
-#PBS -N %s              # job name
-#PBS -l walltime=%s     # duration of the job
-#PBS -l nodes=%d:ppn=%d # number of nodes and cores per node
-#PBS -q %s              # queue name (where job is submitted)
-#PBS -j oe              # combine output and error messages into a single file
-#PBS -o %s              # output file name
-
+#PBS -N %s                 # job name
+#PBS -l walltime=%s        # duration of the job
+#PBS -l nodes=%d:ppn=%d    # number of nodes and cores per node
+#PBS -q %s                 # queue name (where job is submitted)
+#PBS -j oe                 # combine output and error messages into a single file
+#PBS -o %s                 # output file name
 '''
     PROCS_PER_NODE = 24
     output = args.output if args.output is not None else args.name + '.out'
     preamble = preamble_format % (args.name, args.time, args.nodes, PROCS_PER_NODE, args.queue, output)
+    if args.depends:
+        preamble += \
+'#PBS -W depend=afterok:%s  # job ID of the job on which this job depends' % args.depend
+    preamble += '\n'
 
     with NamedTemporaryFile(mode='w', suffix='.sh', delete=False) as pbs:
         pbs.write(preamble)
