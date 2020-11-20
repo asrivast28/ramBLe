0a418052fa1e466eb17637262f851bfbb79fa2af
Ankit Srivastava <asrivast28@gmail.com>
2020-11-19 19:04:08 -0500

    Changes in the PBS script for Phoenix.

--- scripts/pbs.py
+++ scripts/pbs.py
@@ -25,22 +25,36 @@ def parse_args():
     Parse command line arguments.
     '''
     import argparse
-    import multiprocessing
+    from multiprocessing import cpu_count
     import os
     from os.path import abspath, dirname
+    from socket import gethostname
 
     parser = argparse.ArgumentParser(description='Submit custom script using PBS')
     parser.add_argument('-s', '--script', type=str, metavar='FILE', required=True, help='Name of the script to be submitted.')
     parser.add_argument('-n', '--name', type=str, default='benchmark-ramble', metavar='JOB', help='Name of the job.')
     parser.add_argument('-t', '--time', type=str, default='12:00:00', metavar='HH:MM:SS', help='Duration of the job.')
     parser.add_argument('-N', '--nodes', type=int, default=1, metavar='NODES', help='Number of required nodes.')
-    parser.add_argument('-p', '--procs', type=int, default=multiprocessing.cpu_count(), metavar='PROCS', help='Number of processes per node.')
-    parser.add_argument('-q', '--queue', type=str, default='hive', metavar='NAME', help='Name of the queue.')
+    parser.add_argument('-p', '--procs', type=int, default=cpu_count(), metavar='PROCS', help='Number of processes per node.')
+    parser.add_argument('-A', '--account', type=str, metavar='NAME', help='Name of the account.')
+    parser.add_argument('-q', '--queue', type=str, metavar='NAME', help='Name of the queue.')
     parser.add_argument('-o', '--output', type=str, metavar='FILE', help='Name of the output file.')
     parser.add_argument('-d', '--depend', type=str, metavar='JOBID', help='ID of the job on which this job depends.')
     parser.add_argument('-a', '--after', type=str, metavar='HHMM', help='Schedule the job after the given time.')
     parser.add_argument('-v', '--variables', type=str, nargs='*', default=[], metavar='VAR=VALUE', help='Variables to pass to the run script.')
     args = parser.parse_args()
+    if 'hive' in gethostname():
+        # Hive specific defaults
+        if args.queue is None:
+            args.queue = 'hive'
+    if 'phoenix' in gethostname():
+        # Phoenix specific defaults
+        if args.account is None:
+            args.account = 'GT-saluru8-CODA20'
+        if args.queue is None:
+            args.queue = 'inferno'
+    if args.queue is None and args.account is None:
+        raise argparse.ArgumentTypeError('Could not determine queue and/or account information for the job')
     return args
 
 
@@ -63,12 +77,13 @@ def create_submission_script(args):
     output = args.output if args.output is not None else args.name + '.out'
     if os.path.exists(output):
         raise RuntimeError('Output file %s already exists' % output)
+    if args.account:
+        # Account info needs to go before queue info
+        preamble_lines.insert(1, '#PBS -A %s\t# account name' % args.account)
     if args.depend:
         preamble_lines.append('#PBS -W depend=afterok:%s\t# job ID of the job on which this job depends' % args.depend)
     if args.after:
         preamble_lines.append('#PBS -a %s\t# delay executing the job until the given time and date' % args.after)
-    if args.queue == 'hive-priority':
-        preamble_lines.append('#PBS -l advres=asrivastava64.97\t# hive-priority specific argument')
     preamble = '\n'.join(preamble_lines) % (args.name, args.time, args.nodes, args.procs, args.queue, output) + '\n'
 
     with NamedTemporaryFile(mode='w', suffix=os.path.splitext(args.script)[1], delete=False) as pbs:
